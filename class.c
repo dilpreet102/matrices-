08a1ddb91f489ede473a091a2c2be27fe0d13cca
Dilpreet Singh <83696202+dilpreet102@users.noreply.github.com>
2021-06-17 20:29:54 +0530

    Update class.c

--- class.c
+++ class.c
@@ -108,15 +108,24 @@ int main()
             }
 
             break;
-
-        case 5:
+    case 5:
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    c[i][j] = a[i][j] * b[i][j];
+                    c[i][j] = 0;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        c[i][j] += (a[i][k] * b[k][j]);
+                    }
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
                     printf("%d", c[i][j]);
-                    printf("\t");
+                    printf(" ");
                 }
                 printf("\n");
             }
